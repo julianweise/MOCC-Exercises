f3ee696a0e808450640f0a32cb14d830b5b112fd
Julian Weise <privat@julianweise.de>
2018-12-14 10:01:29 +0100

    fix small syntax error

--- exercise2/startAll.sh
+++ exercise2/startAll.sh
@@ -11,14 +11,14 @@ for i in `seq 1 48`;
 do
 	echo $(date +\%s)","$(./measure-cpu.sh) >> "results/${ENVIRONMENT}-cpu.csv"
 	clear_caches
-	sleep TIME_TO_WAIT_BETWEEN
+	sleep $TIME_TO_WAIT_BETWEEN
 	echo $(date +\%s)","$(./measure-mem.sh) >> "results/${ENVIRONMENT}-mem.csv"
 	clear_caches
-	sleep TIME_TO_WAIT_BETWEEN
+	sleep $TIME_TO_WAIT_BETWEEN
 	echo $(date +\%s)","$(./measure-disk-random.sh) >> "results/${ENVIRONMENT}-disk-random.csv"
 	clear_caches
-	sleep TIME_TO_WAIT_BETWEEN
+	sleep $TIME_TO_WAIT_BETWEEN
 	echo $(date +\%s)","$(./measure-fork.sh) >> "results/${ENVIRONMENT}-fork.csv"
 	clear_caches
-	sleep TIME_TO_WAIT_BETWEEN
+	sleep $TIME_TO_WAIT_BETWEEN
 done
